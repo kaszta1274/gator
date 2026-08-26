68ef3fb15043bd32784c7b705cbc2155518b0915
kaszta1274 <62752403+kaszta1274@users.noreply.github.com>
2026-08-26 13:11:10 +0200

    add users reset command

---
+++ handler_reset.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.Args) != 0 {
+		return fmt.Errorf("usage: %s", cmd.Name)
+	}
+
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+
+	fmt.Println("Database reset successfully!")
+	return nil
+}
