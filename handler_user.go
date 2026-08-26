1a18b6b4ba74eae252569798891d1ace7171c508
kaszta1274 <62752403+kaszta1274@users.noreply.github.com>
2026-08-26 13:55:00 +0200

    add users listing command

--- handler_user.go
+++ handler_user.go
@@ -69,3 +69,20 @@ func handlerRegister(s *state, cmd command) error {
 	fmt.Printf("User %s was created!\n%+v\n", userName, user)
 	return nil
 }
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get users: %w", err)
+	}
+
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %s (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %s\n", user.Name)
+		}
+	}
+
+	return nil
+}
