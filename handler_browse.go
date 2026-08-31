ed2cb0c9877c34fe3641d9fb6a5ba02b6e25d3e1
kaszta1274 <62752403+kaszta1274@users.noreply.github.com>
2026-08-31 19:16:19 +0200

    add posts

---
+++ handler_browse.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/kaszta1274/gator/internal/database"
+)
+
+func handlerBrowse(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 0 && len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s [<limit>] - limit defaults to 2", cmd.Name)
+	}
+
+	limit := 2
+	if len(cmd.Args) == 1 {
+		var err error
+		limit, err = strconv.Atoi(cmd.Args[0])
+		if err != nil {
+			return fmt.Errorf("couldn't convert limit to int: %w", err)
+		}
+		if limit < 0 {
+			return fmt.Errorf("limit should be greater than 0")
+		}
+	}
+
+	posts, err := s.db.GetPostsForUser(
+		context.Background(),
+		database.GetPostsForUserParams{
+			UserID: user.ID,
+			Limit:  int32(limit),
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("couldn't get posts for user: %w", err)
+	}
+
+	if len(posts) == 0 {
+		fmt.Printf("No posts found for %s\n", user.Name)
+		return nil
+	}
+
+	fmt.Printf("Found %d posts for %s:\n", len(posts), user.Name)
+	for _, post := range posts {
+		printPost(post)
+		fmt.Println()
+	}
+	return nil
+}
+
+func printPost(post database.Post) {
+	fmt.Printf("* Title: %s\n", post.Title)
+	fmt.Printf("* URL: %s\n", post.Url)
+	fmt.Printf("* Published at: %v\n", post.PublishedAt.Time)
+	fmt.Printf("* Description: %s\n", post.Description.String)
+}
--- rss_feed.go
+++ rss_feed.go
@@ -10,7 +10,10 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/kaszta1274/gator/internal/database"
+	"github.com/lib/pq"
+	"github.com/lib/pq/pqerror"
 )
 
 type RSSFeed struct {
@@ -75,7 +78,7 @@ func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 func scrapeFeeds(s *state) {
 	feed, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
-		fmt.Printf("couldn't get feed to fetch: %v", err)
+		fmt.Printf("couldn't get feed to fetch: %v\n", err)
 		return
 	}
 
@@ -83,24 +86,60 @@ func scrapeFeeds(s *state) {
 		context.Background(),
 		database.MarkFeedFetchedParams{
 			ID:            feed.ID,
-			LastFetchedAt: sql.NullTime{Time: time.Now().UTC()},
+			LastFetchedAt: sql.NullTime{Time: time.Now().UTC(), Valid: true},
 			UpdatedAt:     time.Now().UTC(),
 		},
 	)
 	if err != nil {
-		fmt.Printf("couldn't mark feed as fetched: %v", err)
+		fmt.Printf("couldn't mark feed as fetched: %v\n", err)
 		return
 	}
 
 	rssFeed, err := fetchFeed(context.Background(), feed.Url)
 	if err != nil {
-		fmt.Printf("couldn't fetch feed: %v", err)
+		fmt.Printf("couldn't fetch feed: %v\n", err)
 		return
 	}
 
-	fmt.Printf("%s:\n", rssFeed.Channel.Title)
 	for _, item := range rssFeed.Channel.Item {
-		fmt.Printf("* %s\n", item.Title)
+		pubDate, err := parseDate(item.PubDate)
+		publishedAt := sql.NullTime{Time: pubDate, Valid: true}
+		if err != nil {
+			fmt.Printf("couldn't parse publication date: %v\n", err)
+			publishedAt.Valid = false
+		}
+
+		_, err = s.db.CreatePost(
+			context.Background(),
+			database.CreatePostParams{
+				ID:          uuid.New(),
+				CreatedAt:   time.Now().UTC(),
+				UpdatedAt:   time.Now().UTC(),
+				Title:       item.Title,
+				Url:         item.Link,
+				Description: sql.NullString{String: item.Description, Valid: true},
+				PublishedAt: publishedAt,
+				FeedID:      feed.ID,
+			},
+		)
+		uniqueViolationErr := pq.As(err, pqerror.UniqueViolation)
+		if uniqueViolationErr != nil {
+			continue
+		}
+		if err != nil {
+			fmt.Printf("couldn't create post: %v\n", err)
+		}
 	}
-	fmt.Println()
+}
+
+func parseDate(date string) (time.Time, error) {
+	formats := []string{time.RFC1123, time.RFC1123Z, time.RFC3339}
+
+	for _, format := range formats {
+		parsed, err := time.Parse(format, date)
+		if err == nil {
+			return parsed, nil
+		}
+	}
+	return time.Time{}, fmt.Errorf("couldn't match date format for %s", date)
 }
